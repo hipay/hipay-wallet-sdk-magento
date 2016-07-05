b13d78391668e8bb7513f3f8d0734af9cc0903d7
Johan Protin <jprotin@hipay.com>
2016-07-05 11:07:05 +0200

    update rating values +12,+16,+18

--- app/code/local/HimediaPayments/Hipay/Model/Rating.php
+++ app/code/local/HimediaPayments/Hipay/Model/Rating.php
@@ -11,9 +11,9 @@ class HimediaPayments_Hipay_Model_Rating
     {
         return array(
             array('value'=>'ALL', 'label'=>Mage::helper('hipaymod')->__('For all ages')),
-            array('value'=>'12+', 'label'=>Mage::helper('hipaymod')->__('For ages 12 and over')),
-            array('value'=>'16+', 'label'=>Mage::helper('hipaymod')->__('For ages 16 and over')),
-            array('value'=>'18+', 'label'=>Mage::helper('hipaymod')->__('For ages 18 and over')),
+            array('value'=>'+12', 'label'=>Mage::helper('hipaymod')->__('For ages 12 and over')),
+            array('value'=>'+16', 'label'=>Mage::helper('hipaymod')->__('For ages 16 and over')),
+            array('value'=>'+18', 'label'=>Mage::helper('hipaymod')->__('For ages 18 and over')),
         );
     }
 
